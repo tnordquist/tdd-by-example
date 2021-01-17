5888820743f47b48de887048b2ff3b88025bd17e
tnordquist <toddnordquist144@gmail.com>
2021-01-16 20:43:05 -0700

    Added equals() for testing the value object: Dollar.

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,6 +1,7 @@
 package guru.springframework;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,4 +15,11 @@ public class MoneyTest {
     product = five.times(3);
     assertEquals(15, product.amount);
   }
+
+  @Test
+  void testEquality() {
+    assertEquals(new Dollar(5), new Dollar(5));
+    assertNotEquals(new Dollar(5), new Dollar(8));
+  }
 }
+
