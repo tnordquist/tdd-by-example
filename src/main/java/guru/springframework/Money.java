c564bc94199c6be40ac4cceee550b9f0fde055d2
tnordquist <toddnordquist144@gmail.com>
2021-01-18 21:37:30 -0700

    Compare Francs with Dollars

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -7,6 +7,6 @@ public class Money {
   @Override
   public boolean equals(Object obj) {
     Money money = (Money) obj;
-    return amount == money.amount;
+    return amount == money.amount && this.getClass().equals(obj.getClass());
   }
 }
